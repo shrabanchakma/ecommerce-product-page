9e3f4bc40f527758d98a18ade40fae5c683208d2
Shraban Chakma <shrabanchakma328@gmail.com>
2024-03-04 01:19:55 +0600

    initial commit

---
+++ src/pages/productPage/ProductPage/ProductPage.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../../../Components/Shared/Navbar/Navbar";
+const ProductPage = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1 className="">this is the main section</h1>
+    </div>
+  );
+};
+
+export default ProductPage;
