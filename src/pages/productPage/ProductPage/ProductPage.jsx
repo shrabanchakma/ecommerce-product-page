dedc8f17c8b8f944948f8fd87c4ec0c853fa9051
Shraban Chakma <shrabanchakma328@gmail.com>
2024-03-04 03:45:21 +0600

    added hover effect on menu items

--- src/pages/productPage/ProductPage/ProductPage.jsx
+++ src/pages/productPage/ProductPage/ProductPage.jsx
@@ -1,7 +1,7 @@
 import Navbar from "../../../Components/Shared/Navbar/Navbar";
 const ProductPage = () => {
   return (
-    <div>
+    <div className="max-w-screen-xl mx-auto h-[100vh] bg-pink-500">
       <Navbar />
       <h1 className="">this is the main section</h1>
     </div>
