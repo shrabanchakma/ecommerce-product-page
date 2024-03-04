029fb83970ad104e6d279e843898b72068db38b4
Shraban Chakma <shrabanchakma328@gmail.com>
2024-03-05 03:29:35 +0600

    added thumbnails

--- src/pages/productPage/ProductInfoSection/ProductGallary.jsx
+++ src/pages/productPage/ProductInfoSection/ProductGallary.jsx
@@ -1,5 +1,8 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import { productImages } from "../../../Components/Shared/Images/Images";
+import {
+  productImages,
+  productThumbnailImages,
+} from "../../../Components/Shared/Images/Images";
 import { FreeMode, Navigation, Thumbs } from "swiper/modules";
 import { useState } from "react";
 const ProductGallery = () => {
@@ -17,17 +20,42 @@ const ProductGallery = () => {
       </>
     );
   };
+
+  const getProductThumbnailImages = () => {
+    return (
+      <>
+        {productThumbnailImages.map((imageItem, idx) => (
+          <SwiperSlide key={idx}>
+            <img src={imageItem} alt={imageItem} />
+          </SwiperSlide>
+        ))}
+      </>
+    );
+  };
+
   return (
     <div>
       <Swiper
         modules={[FreeMode, Navigation, Thumbs]}
         spaceBetween={10}
-        navigation={true}
         loop={true}
-        className="bg-blue-500"
-        // thumbs={{swiper: thumbsSwiper}}
+        className="bg-blue-500 w-1/2 rounded-2xl"
+        thumbs={{
+          swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
+        }}
       >
         {getProductImages()}
+      </Swiper>
+      <Swiper
+        modules={[FreeMode, Navigation, Thumbs]}
+        onSwiper={setThumbsSwiper}
+        spaceBetween={0}
+        slidesPerView={4}
+        freeMode={true}
+        watchSlidesProgress={true}
+        className="bg-green-500 w-1/2"
+      >
+        {getProductThumbnailImages()}
       </Swiper>
     </div>
   );
