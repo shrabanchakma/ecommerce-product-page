5e7d1a309c95bb4f2706064c427a6a1df915aa30
Shraban Chakma <shrabanchakma328@gmail.com>
2024-03-06 16:15:37 +0600

    slider added with custom react

--- src/pages/productPage/ProductInfoSection/ProductGallary.jsx
+++ src/pages/productPage/ProductInfoSection/ProductGallary.jsx
@@ -1,5 +1,38 @@
+import { useState } from "react";
+import data from "../../../constants/Images";
 const ProductGallary = () => {
-  return <div>image gallery</div>;
+  const products = [...data];
+  const [value, setValue] = useState(0);
+  const currentImg = products[value].largeImg;
+  const getThumbnails = () => {
+    return (
+      <>
+        {products.map((product, idx) => (
+          <img
+            onClick={() => setValue(idx)}
+            key={idx}
+            src={product.smallImg}
+            alt={product.smallImg}
+            className="rounded-lg w-[6.8rem] hover:border-[3px] border-orange transition-all hover:scale-105 hover:opacity-50 hover:cursor-pointer"
+          />
+        ))}
+      </>
+    );
+  };
+  return (
+    <div className="w-1/2 bg-green-500">
+      <div className=" bg-blue-500 hover:cursor-pointer hover:opacity-90 transition ease-in delay-75">
+        <img
+          src={currentImg}
+          alt={products[0].largeImg}
+          className="rounded-3xl"
+        />
+      </div>
+      <div className="flex justify-around mt-7 w-[36rem] ">
+        {getThumbnails()}
+      </div>
+    </div>
+  );
 };
 
 export default ProductGallary;
