49fa4d6030fb1244935e2fbc043a0bafe994b45d
Shraban Chakma <shrabanchakma328@gmail.com>
2024-03-06 23:04:10 +0600

    added funtionality to go to next or prev image in lightbox

--- src/pages/productPage/ProductInfoSection/ProductGallary.jsx
+++ src/pages/productPage/ProductInfoSection/ProductGallary.jsx
@@ -14,16 +14,27 @@ const ProductGallary = () => {
     return (
       <>
         {products.map((product, idx) => (
-          <img
-            onClick={() => setValue(idx)}
-            key={idx}
-            src={product.smallImg}
-            alt={product.smallImg}
-            className="rounded-lg w-[6.8rem] hover:border-[3px] border-orange transition-all hover:scale-105 hover:opacity-50 hover:cursor-pointer"
-          />
+          <div key={idx} className="bg-white rounded-lg">
+            <img
+              onClick={() => setValue(idx)}
+              key={idx}
+              src={product.smallImg}
+              alt={product.smallImg}
+              className=" rounded-lg w-[6.8rem] hover:border-[3px] border-orange transition-all hover:scale-105 hover:opacity-50 hover:cursor-pointer"
+            />
+          </div>
         ))}
       </>
     );
+  };
+
+  const goNext = () => {
+    console.log("go next working");
+    setValue(value === products.length - 1 ? 0 : value + 1);
+  };
+  const goPrev = () => {
+    console.log("got prev working");
+    setValue(value === 0 ? products.length - 1 : value - 1);
   };
   return (
     <div className="w-1/2">
@@ -39,20 +50,38 @@ const ProductGallary = () => {
       <div
         className={`${
           modal ? "hidden" : "hidden md:block"
-        } absolute  right-0 -top-0 bottom-0 left-0 bg-lightBlack z-10`}
+        } absolute  inset-0 bg-lightBlack z-10`}
       >
         <div className="w-[35rem] flex flex-col items-center absolute  top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] ">
-          <div className=" bg-blue-500 hover:cursor-pointer hover:opacity-90 transition ease-in delay-75  ">
-            <img src="" alt="" />
-
+          <div className=" hover:cursor-pointer transition ease-in delay-75 relative">
             <img
               onClick={() => setModal((prev) => !prev)}
-              src={currentImg}
-              alt={products[0].largeImg}
-              className="rounded-3xl "
+              src={closeIcon}
+              alt={closeIcon}
+              className="w-[1.8rem] absolute right-0 -top-10 z-20 transition-all hover:scale-110"
+            />
+            <img
+              onClick={goPrev}
+              src={prevIcon}
+              alt={prevIcon}
+              className="h-[3.5rem] w-[3.5rem] p-5 rounded-full bg-white absolute -left-6 top-[50%] -translate-y-[50%] z-10 transition-all hover:scale-110"
+            />
+            <div className="bg-white rounded-3xl">
+              <img
+                onClick={() => setModal((prev) => !prev)}
+                src={currentImg}
+                alt={products[0].largeImg}
+                className="rounded-3xl hover:opacity-95"
+              />
+            </div>
+            <img
+              onClick={goNext}
+              src={nextIcon}
+              alt={nextIcon}
+              className="h-[3.5rem] w-[3.5rem] p-5 rounded-full bg-white absolute -right-6 top-[50%] -translate-y-[50%] z-10 transition-all hover:scale-110"
             />
           </div>
-          <div className="flex justify-around mt-7 w-[36rem] ">
+          <div className="flex justify-around mt-7 w-[36rem]  ">
             {getThumbnails()}
           </div>
         </div>
